7264eaf7cae1131031696f85c526fe8f446918d8
ubuntu030 <ubuntu030@gmail.com>
2021-02-08 14:01:22 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { render } from "react-dom";
+
+import "./style.scss";
+
+function App() {
+	return (
+		<div>
+			Hello
+		</div>
+	)
+
+}
+
+render(<App />, document.getElementById("root"));
