3b581aaa30414679d7852f5e74db38176f18f69c
ubuntu030 <ubuntu030@gmail.com>
2021-02-09 11:46:01 +0800

    import Gmap

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { render } from "react-dom";
 
+import Gmap from "./components/Gmap";
+
 import "./style.scss";
 
 function App() {
 	return (
 		<div>
-			Hello
+			<Gmap />
 		</div>
 	)
 
