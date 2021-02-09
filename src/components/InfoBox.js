12c9a2c74f02a252d0d7d2148ca3f5e9078b393a
ubuntu030 <ubuntu030@gmail.com>
2021-02-09 20:48:28 +0800

    add InfoBox and its style

---
+++ src/components/InfoBox.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import "./InfoBox.scss";
+
+const InfoBox = () => {
+
+
+	return (
+		<div className="info-box-container">
+			info-box
+		</div>
+	)
+}
+
+export default InfoBox;
