1424856b995663f5ec053ae1b8a642aca4055d1c
ubuntu030 <ubuntu030@gmail.com>
2021-02-10 20:08:11 +0800

    update infoBox and its style

--- src/components/InfoBox.js
+++ src/components/InfoBox.js
@@ -1,16 +1,27 @@
 import React, { useState } from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+
+import { fetchPopInfoSuccess } from "../actions";
 import "./InfoBox.scss";
 
+import { findPlace } from "./Gmap";
+
 const InfoBox = () => {
-	const { markList } = useSelector(state => state);
+	const dispatch = useDispatch();
+	const { markList } = useSelector(state => state.infoBoxRdcr);
 	console.log(markList);
+	const _onClick = async (item) => {
+		const result = await findPlace(item.title);
+		console.log(result);
+		dispatch(fetchPopInfoSuccess(result));
+	}
+
 	return (
 		<div className="info-box-container">
 			<ul>
 				{
 					markList.map(item => (
-						<li key={item.title}>{item.title}</li>
+						<li key={item.title} onClick={() => _onClick(item)}>{item.title}</li>
 					))
 				}
 			</ul>
