3e759660cfecb3408087a4589b2807aaec4d5979
ubuntu030 <ubuntu030@gmail.com>
2021-02-10 21:26:06 +0800

    add padding action

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,7 +4,9 @@
 export const SET_MARKS_LIST = 'SET_MARKS_LIST';
 
 export const IS_SHOW_POPBOX = 'IS_SHOW_POPBOX'
+export const FETCH_POP_INFO_PADDING = 'FETCH_POP_INFO_PADDING';
 export const FETCH_POP_INFO_SUCCESS = 'FETCH_POP_INFO_SUCCESS';
+export const FETCH_POP_INFO_ERROR = 'FETCH_POP_INFO_ERROR';
 /**
  * INFOBOX ACTION
  */
@@ -17,6 +19,11 @@ export const setMarksList = (list) => {
 /**
  * POPBOX ACTION
  */
+export const fetchPopInfoPadding = () => {
+	return {
+		type: FETCH_POP_INFO_PADDING,
+	}
+}
 export const fetchPopInfoSuccess = (info) => {
 	return {
 		type: FETCH_POP_INFO_SUCCESS,
