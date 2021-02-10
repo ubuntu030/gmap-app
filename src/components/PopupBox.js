519a02bf1a6d4fabcaecfe6b6cfc978aebc4d992
ubuntu030 <ubuntu030@gmail.com>
2021-02-10 20:50:50 +0800

    - display info of store when client click item on list - update style

--- src/components/PopupBox.js
+++ src/components/PopupBox.js
@@ -8,16 +8,42 @@ import "./PopupBox.scss";
 const PopupBox = () => {
 	const dispatch = useDispatch();
 	const { isShow, info } = useSelector(state => state.popBoxRdcr);
-	return (
-		<main className="popup-box">
-			<section className="header card">
-				header
-			</section>
-			<section className="container card">
-				container
-			</section>
-		</main>
-	)
+	return (isShow && info && typeof info === 'object') ?
+		createTemplate(info)
+		: null
 }
+
+const createTemplate = (info) => (
+	<main className="popup-box">
+		<section className="header card">
+			<h1>{info.name}</h1>
+		</section>
+		<section className="container card">
+			<div>
+				<label>地址:</label>
+				{info.formatted_address}
+			</div>
+			<div>
+				<label>評價:</label>
+				{info.rating}
+			</div>
+			<div>
+				<label>圖片:</label>
+				<ul>
+					{
+						info.photos.map((item, i) => {
+							return (
+								<li key={i}>
+									<img src={item.getUrl()} alt="" />
+								</li>
+							)
+						})
+					}
+
+				</ul>
+			</div>
+		</section>
+	</main>
+);
 
 export default PopupBox;
