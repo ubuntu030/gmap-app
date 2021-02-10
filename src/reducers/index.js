6b892eb1c308442e7ce099dbd37bb90eb5e281fd
ubuntu030 <ubuntu030@gmail.com>
2021-02-10 20:07:21 +0800

    combine reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,14 +1,10 @@
-const initialState = {
-	markList: []
-};
+import { combineReducers } from "redux";
+import infoBoxRdcr from "./infoBoxRdcr";
+import popBoxRdcr from "./popBoxRdcr";
 
-const rootReducer = (state = initialState, action) => {
-	switch (action.type) {
-		case 'SET_MARKS_LIST':
-			return { ...state, markList: [...action.payload] }
-		default:
-			return state;
-	}
-}
-
+// NOTICE: combineReducers have to use multiple reducer,otherwise it can't do initial state when I tyr to useSelector in component first time.
+const rootReducer = combineReducers({
+	infoBoxRdcr,
+	popBoxRdcr
+});
 export default rootReducer;
