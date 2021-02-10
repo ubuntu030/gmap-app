42a8d6dc23cd943f4ac476743990e2ac914ba5e1
ubuntu030 <ubuntu030@gmail.com>
2021-02-10 20:09:11 +0800

    add new action and action's type

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,32 @@
-const SET_MARKS_LIST = 'SET_MARKS_LIST';
+/**
+ * ACTION TYPE
+ */
+export const SET_MARKS_LIST = 'SET_MARKS_LIST';
 
+export const IS_SHOW_POPBOX = 'IS_SHOW_POPBOX'
+export const FETCH_POP_INFO_SUCCESS = 'FETCH_POP_INFO_SUCCESS';
+/**
+ * INFOBOX ACTION
+ */
 export const setMarksList = (list) => {
 	return {
 		type: SET_MARKS_LIST,
 		payload: list
 	}
 }
+/**
+ * POPBOX ACTION
+ */
+export const fetchPopInfoSuccess = (info) => {
+	return {
+		type: FETCH_POP_INFO_SUCCESS,
+		payload: info
+	}
+}
+export const isShowPopbox = (isShow) => {
+	return {
+		type: IS_SHOW_POPBOX,
+		payload: isShow
+	}
+}
+
