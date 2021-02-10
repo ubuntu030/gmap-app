dddbc4d11dce77718b6933f2c8984a4454724511
ubuntu030 <ubuntu030@gmail.com>
2021-02-10 21:26:16 +0800

    add padding reducer

--- src/reducers/popBoxRdcr.js
+++ src/reducers/popBoxRdcr.js
@@ -1,15 +1,18 @@
-import { IS_SHOW_POPBOX, FETCH_POP_INFO_SUCCESS } from "../actions";
+import { IS_SHOW_POPBOX, FETCH_POP_INFO_SUCCESS,FETCH_POP_INFO_PADDING } from "../actions";
 const initialState = {
 	isShow: false,
-	info: null
+	info: null,
+	loading: false,
 };
 
 const rootReducer = (state = initialState, action) => {
 	switch (action.type) {
 		case IS_SHOW_POPBOX:
-			return { ...state }
+			return { ...state, isShow: action.payload }
+		case FETCH_POP_INFO_PADDING:
+			return { ...state, loading: true }
 		case FETCH_POP_INFO_SUCCESS:
-			return { ...state, isShow: true, info: action.payload[0] }
+			return { ...state, isShow: true, info: action.payload[0], loading: false }
 		default:
 			return state;
 	}
