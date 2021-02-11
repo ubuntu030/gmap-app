ed45b69046760a6dff562ae801b27cf11c7d13f5
ubuntu030 <ubuntu030@gmail.com>
2021-02-11 11:27:11 +0800

    update

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,11 +2,16 @@
  * ACTION TYPE
  */
 export const SET_MARKS_LIST = 'SET_MARKS_LIST';
+export const SET_MY_LOC_PADDING = 'SET_MY_LOC_PADDING';
+export const SET_MY_LOC_SUCCESS = 'SET_MY_LOC_SUCCESS';
+export const DEL_MY_LOC = 'DEL_MY_LOC';
 
 export const IS_SHOW_POPBOX = 'IS_SHOW_POPBOX'
 export const FETCH_POP_INFO_PADDING = 'FETCH_POP_INFO_PADDING';
 export const FETCH_POP_INFO_SUCCESS = 'FETCH_POP_INFO_SUCCESS';
 export const FETCH_POP_INFO_ERROR = 'FETCH_POP_INFO_ERROR';
+
+
 /**
  * INFOBOX ACTION
  */
@@ -14,6 +19,12 @@ export const setMarksList = (list) => {
 	return {
 		type: SET_MARKS_LIST,
 		payload: list
+	}
+}
+export const setMyLocation = (data) => {
+	return {
+		type: 'SET_MY_LOC_SUCCESS',
+		payload: data
 	}
 }
 /**
