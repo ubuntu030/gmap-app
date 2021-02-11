59318b1025e6275b7ebae8c1115eaab7e6af5eaa
ubuntu030 <ubuntu030@gmail.com>
2021-02-11 11:27:55 +0800

    reducer process

--- src/reducers/infoBoxRdcr.js
+++ src/reducers/infoBoxRdcr.js
@@ -1,13 +1,21 @@
-import { SET_MARKS_LIST } from "../actions";
+import { SET_MARKS_LIST, SET_MY_LOC_PADDING, SET_MY_LOC_SUCCESS, DEL_MY_LOC } from "../actions";
 
 const initialState = {
-	markList: []
+	markList: [],
+	myLocationOnSetting: false,
+	myLocation: null
 };
 
 const rootReducer = (state = initialState, action) => {
 	switch (action.type) {
 		case SET_MARKS_LIST:
 			return { ...state, markList: [...action.payload] }
+		case SET_MY_LOC_PADDING:
+			return { ...state, myLocationOnSetting: true }
+		case SET_MY_LOC_SUCCESS:
+			return { ...state, myLocationOnSetting: false, my_location: action.payload }
+		case DEL_MY_LOC:
+			return { ...state, my_location: null }
 		default:
 			return state;
 	}
