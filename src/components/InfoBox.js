61e1516ea4eff61318c27a8782cd69099964b6bc
ubuntu030 <ubuntu030@gmail.com>
2021-02-11 22:33:36 +0800

    版面調整

--- src/components/InfoBox.js
+++ src/components/InfoBox.js
@@ -4,7 +4,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { fetchPopInfoSuccess, fetchPopInfoPadding } from "../actions";
 import "./InfoBox.scss";
 
-import { findPlace, routeRender } from "./Gmap";
+import { findPlace, routeRender, cleanDirectionsRenderer } from "./Gmap";
 
 const InfoBox = () => {
 	const dispatch = useDispatch();
@@ -22,21 +22,33 @@ const InfoBox = () => {
 			routeRender(p1, p2);
 		}
 	}
+	const cleanRoute = () => cleanDirectionsRenderer();
 
 	return (
 		<div className="info-box-container">
-			<div className="my-loc-ctn">
-				<div>
+			<div className="location-ctn">
+				<div className="coordinate">
 					{myLocation ? myLocation.lat().toFixed(3) + ',' + myLocation.lng().toFixed(3) : null}
+				</div>
+				<div>
+					<button onClick={() => cleanRoute()}>清除路徑</button>
 				</div>
 			</div>
 			<ul>
 				{
 					markList.map(item => (
-						<li key={item.title} onClick={() => storeClick(item)}>
-							<p>{item.title}</p>
-							<p>{item.distance ? Math.round(item.distance) + '米' : null}</p>
-							<button onClick={() => routeClick(myLocation, item.position)}>導航</button>
+						<li key={item.title}>
+							<div className="title" onClick={() => storeClick(item)}>
+								{item.title}
+							</div>
+							<div className="loc-relation-ctn">
+								<div>
+									{item.distance ? Math.round(item.distance) + '米' : null}
+								</div>
+								<div>
+									<button onClick={() => routeClick(myLocation, item.position)}>導航</button>
+								</div>
+							</div>
 						</li>
 					))
 				}
