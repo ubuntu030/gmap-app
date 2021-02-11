bb9a8ccd63e2e0a2d228b067e9eb58ed1ff6d1af
ubuntu030 <ubuntu030@gmail.com>
2021-02-11 14:55:08 +0800

    update state of sorting distance

--- src/reducers/infoBoxRdcr.js
+++ src/reducers/infoBoxRdcr.js
@@ -1,4 +1,4 @@
-import { SET_MARKS_LIST, SET_MY_LOC_PADDING, SET_MY_LOC_SUCCESS, DEL_MY_LOC } from "../actions";
+import { SET_MARKS_LIST, SET_MY_LOC_PADDING, SET_MY_LOC_SUCCESS, DEL_MY_LOC, SORT_BY_DISTANCE } from "../actions";
 
 const initialState = {
 	markList: [],
@@ -13,6 +13,8 @@ const rootReducer = (state = initialState, action) => {
 			return { ...state }
 		case SET_MY_LOC_SUCCESS:
 			return { ...state, myLocation: action.payload }
+		case SORT_BY_DISTANCE:
+			return { ...state, markList: action.payload.sort((a, b) => a.distant - b.distant) }
 		case DEL_MY_LOC:
 			return { ...state, myLocation: null }
 		default:
