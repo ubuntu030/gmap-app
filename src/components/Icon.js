4a565e6040422870e73f49c770aad88cf352b7f1
ubuntu030 <ubuntu030@gmail.com>
2021-02-12 14:55:06 +0800

    add icon

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -1,7 +1,10 @@
-const ICON_FOLDER = 'src/public/map-icon/'
+const MAP_ICON_FOLDER = 'src/public/map-icon/'
+const ICON_FOLDER = 'src/public/'
 export default {
-	flag: ICON_FOLDER + 'icon-flag.png',
-	restaurant: ICON_FOLDER + 'snack_bar.png',
-	redCircle: ICON_FOLDER + 'red-circle.png',
-	redDot: ICON_FOLDER + 'red-dot.png'
+	flag: MAP_ICON_FOLDER + 'icon-flag.png',
+	restaurant: MAP_ICON_FOLDER + 'snack_bar.png',
+	redCircle: MAP_ICON_FOLDER + 'red-circle.png',
+	redDot: MAP_ICON_FOLDER + 'red-dot.png',
+	plus: ICON_FOLDER + 'icons8-plus-24.png',
+	minus: ICON_FOLDER + 'icons8-minus-24.png'
 }
