2e7eaa0679db687070afe96009c213b888023db0
ubuntu030 <ubuntu030@gmail.com>
2021-02-12 10:26:09 +0800

    processing state of padding

--- src/reducers/infoBoxRdcr.js
+++ src/reducers/infoBoxRdcr.js
@@ -1,20 +1,21 @@
-import { SET_MARKS_LIST, SET_MY_LOC_PADDING, SET_MY_LOC_SUCCESS, DEL_MY_LOC, SORT_BY_DISTANCE } from "../actions";
+import { SET_MARKS_LIST, SET_MY_LOC_SUCCESS, DEL_MY_LOC, SORT_BY_DISTANCE,SET_MARKS_LIST_PADDING } from "../actions";
 
 const initialState = {
 	markList: [],
-	myLocation: null
+	myLocation: null,
+	isPadding: false
 };
 
 const rootReducer = (state = initialState, action) => {
 	switch (action.type) {
+		case SET_MARKS_LIST_PADDING:
+			return { ...state, isPadding: true }
 		case SET_MARKS_LIST:
-			return { ...state, markList: [...action.payload] }
-		case SET_MY_LOC_PADDING:
-			return { ...state }
+			return { ...state, markList: [...action.payload], isPadding: false }
 		case SET_MY_LOC_SUCCESS:
 			return { ...state, myLocation: action.payload }
 		case SORT_BY_DISTANCE:
-			return { ...state, markList: action.payload.sort((a, b) => a.distant - b.distant) }
+			return { ...state, markList: action.payload.sort((a, b) => a.distance - b.distance) }
 		case DEL_MY_LOC:
 			return { ...state, myLocation: null }
 		default:
