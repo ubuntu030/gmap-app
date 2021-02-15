2c1b45ffad58e62ab1866c3dd984beafaaa22294
ubuntu030 <ubuntu030@gmail.com>
2021-02-15 21:54:36 +0800

    another key

--- src/components/GmapAPI.js
+++ src/components/GmapAPI.js
@@ -1,5 +1,5 @@
-const API_KEY = 'AIzaSyCd6kNCPNNnVZnd45Es3WTY8xfzeYdhUQQ';
-// const API_KEY = 'AIzaSyC2vHCeSU4VcX4I6iu-5tHh4MjWw8fNLFg'
+// const API_KEY = 'AIzaSyCd6kNCPNNnVZnd45Es3WTY8xfzeYdhUQQ';
+const API_KEY = 'AIzaSyC2vHCeSU4VcX4I6iu-5tHh4MjWw8fNLFg'
 
 export const KEY = API_KEY;
 export const URL = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}&libraries=places`;
